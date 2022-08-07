3e873779ef3e70ad10a3b71eb9f63ac1f7f99270
Flora Kho <florakho123@gmail.com>
2022-08-06 22:17:20 -0700

    login-signup form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
+import * as sessionActions from "../../store/session";
+import logo from './logo.png';
+import './LoginForm.css';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -9,6 +12,7 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -30,35 +34,63 @@ const LoginForm = () => {
     return <Redirect to='/notes' />;
   }
 
+  const demouser = async () => {
+    await dispatch(sessionActions.login("demo@aa.io", "password"));
+    history.push('/notes');
+  }
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='loginpage'>
+      <div className='center'>
+        <div>
+          <div>
+            <NavLink to='/'>
+              <img style={{ width: '90px', height: '50px' }} src={logo} alt='MaxNote' />
+              <div style={{ textDecoration: 'none', color: '#333' }}>Maxnote</div>
+            </NavLink>
+          </div>
+          <div>Remember everything important.</div>
+        </div>
+        <div>
+          <button className='demo' onClick={demouser}>Log in as demo user</button>
+        </div>
+        <div className='line'>
+          <div>or</div>
+        </div>
+        <form onSubmit={onLogin}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            {/* <label htmlFor='email'>Email</label> */}
+            <input
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            {/* <label htmlFor='password'>Password</label> */}
+            <input
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+          <button type='submit'>Log In</button>
+        </form>
+        <div>
+          <h4>Don't have an account?</h4>
+          <NavLink to='/sign-up'><div>Create account</div></NavLink>
+        </div>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
+
   );
 };
 
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import logo from './logo.png';
+import './SignUpForm.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -43,51 +45,73 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+
+    <div class='signuppage'>
+      <div class='signupform'>
+        <div>
+          <div>
+            <NavLink to='/'>
+              <img style={{ width: '90px', height: '50px' }} src={logo} alt='MaxNote' />
+              <div style={{ textDecoration: 'none', color: '#333' }}>Maxnote</div>
+            </NavLink>
+          </div>
+          <div>Remember everything important.</div>
+        </div>
+        <form onSubmit={onSignUp}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            {/* <label>Username</label> */}
+            <input
+            placeholder='Username'
+              type='text'
+              name='username'
+              onChange={updateUsername}
+              value={username}
+            ></input>
+          </div>
+          <div>
+            {/* <label>Email</label> */}
+            <input
+            placeholder='Email'
+              type='text'
+              name='email'
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div>
+            {/* <label>Password</label> */}
+            <input
+            placeholder='Password'
+              type='password'
+              name='password'
+              onChange={updatePassword}
+              value={password}
+            ></input>
+          </div>
+          <div>
+            {/* <label>Confirmed Password</label> */}
+            <input
+            placeholder='Confirmed password'
+              type='password'
+              name='repeat_password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+          </div>
+          <button type='submit'>Sign Up</button>
+        </form>
+        <div>
+          <h4>Already have an account?</h4>
+          <NavLink to='/login'><div>Log In</div></NavLink>
+        </div>
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
   );
 };
 
