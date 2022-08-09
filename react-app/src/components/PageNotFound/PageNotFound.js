4508b02b026a12eb81513b53d78150d805c728e4
Flora Kho <florakho123@gmail.com>
2022-08-09 13:38:08 -0700

    search

---
+++ react-app/src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './PageNotFound.css';
+
+
+function PageNotFound() {
+
+    const sessionUser = useSelector((state) => state.session.user);
+
+
+    return (
+
+            <>
+
+                <div className='pagenotfound'>
+                    {sessionUser ? (<Link to='/notes'><button>Go Back</button></Link>) :
+
+                        (<Link to='/'><button>Go Back</button></Link>)}
+                </div>
+
+            </>
+
+    )
+}
+
+export default PageNotFound;
